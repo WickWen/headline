11e2930ae2239d9102e48af67210791a413c207a
WickWen <mrwickweb@163.com>
2020-02-12 02:55:27 +0800

    登录页创建关闭按钮和logo

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,34 @@
 <template>
-  <div>这里是登录页</div>
+  <div class="main">
+    <div class="closeBtn">
+      <span class="iconfont iconicon-test"></span>
+    </div>
+    <div class="logo">
+      <span class="iconfont iconnew"></span>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang='less'>
+.closeBtn {
+  padding: 6.389vw;
+  span {
+    font-size: 7.778vw;
+    color: #0078d7;
+  }
+}
+.logo {
+  text-align: center;
+  span {
+    font-size: 35vw;
+    color: #ff5c67;
+  }
+}
+.main {
+  background-color: #f2f2f2;
+}
 </style>
